640c430811575435856ef1f1f9f94f4ad14c4311
jafet <jafetalberto06@gmail.com>
2023-10-08 19:16:10 -0500

    Firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+
+    // ...
+
+
+    // Add the dependency for the Google services Gradle plugin
+
+    id("com.google.gms.google-services") version "4.4.0" apply false
+
+}
+
 plugins {
     id("com.android.application") version "8.1.1" apply false
 }
